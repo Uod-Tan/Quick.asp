fb22aa7e09fb7ddf2c527935e47f08969f291c74
Uod-Tan <145178111+Uod-Tan@users.noreply.github.com>
2023-09-22 16:57:41 +0800

    Add files via upload
    1st asp.net experience

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication_TRY.Models;
+
+namespace WebApplication_TRY.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var studentInfoArray = new[]
+       {
+            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
+            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+        };
+
+            return View(studentInfoArray);
+        }
+    
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+      
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StudentInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication_TRY.Models
+{
+    public class StudentInfoModel
+    {
+        public string Name { get; set; }
+        public string StudentId { get; set; }
+        public string StudentAge { get; set; }
+    }
+}
